b7217c85ce66c816ace2c5b3ef8452341d24295f
MwitaChacha <jonahhchacha@gmail.com>
2021-08-28 01:50:21 +0300

    Adding of neccessary comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Scroll
 $(document).ready(function () {
     $('#scroll-btn, .product-link, .product-button').on('click', function () {
         $('html, body, .intro-section').animate({
@@ -10,6 +11,7 @@ $(document).ready(function () {
     $('#women-section').hide();
 });
 
+// Men Clothing Section Toggle
 $(document).ready(function () {
     $('#men-toggle').attr('checked', true).show();
     $("#men-toggle").on('click', function () {
@@ -17,11 +19,9 @@ $(document).ready(function () {
     });
 });
 
+// Women Clothing Section toggle
 $(document).ready(function () {
     $("#women-toggle").click(function () {
         $("#women-section").toggle(500);
-
-        
-
     });
 });
